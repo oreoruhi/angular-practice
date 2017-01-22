bc1db76df1ce5d8bc9e05f8dceb0197c362390e7
Rugie Barrameda <norugie015@hotmail.com>
2017-01-22 21:51:17 +0800

    controller tutorial

--- js/app.js
+++ js/app.js
@@ -1,1 +1,44 @@
-angular.module('app', []);
+var app = angular.module('app', []);
+
+// var sexyDJ = function() {
+// 	var truth1 = function() {
+// 		console.log("It is not true that DJ is not sexy.");
+// 	};
+
+// 	var truth2 = function() {
+// 		console.log("It is also not true that he is sexy. So which is which?");
+// 	};
+
+// 	return {
+// 		truthOne: truth1,
+// 		truthTwo: truth2
+// 	};
+// }
+
+
+// var truthpolice = sexyDJ();
+
+// truthpolice.truthOne();
+// truthpolice.truthTwo();
+// console.log(truthpolice);
+
+
+app.controller('MainController', ['$scope', '$http', function ($scope, $http) {
+	
+	$scope.message = "SexyDJ is false";
+
+	var DJ = {
+		firstname: "Denniel Joshu",
+		lastname: "Diaz",
+		desc: "Sexy"
+	}
+
+	$scope.DJ = DJ;
+
+	$http.get("https://api.github.com/users/oreoruhi")
+		 .then(function(response){
+		 	$scope.oreoruhi = response.data;
+		 	console.log(response);
+		 });
+
+}]);
